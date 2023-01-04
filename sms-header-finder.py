0ca2197319be114c98542095210940f5bb198c8d
mike <barbeque@users.noreply.github.com>
2023-01-04 15:57:59 -0700

    Fix the bug so it actually looks for it

--- sms-header-finder.py
+++ sms-header-finder.py
@@ -7,16 +7,19 @@ if len(sys.argv) < 2:
 junk = []
 with open(sys.argv[1], 'rb') as f:
 	junk = f.read()
+print('ROM length', len(junk))
 
 POSSIBLE_OFFSETS = [0x1ff0, 0x3ff0, 0x7ff0]
 found_it = False
-for offset in POSSIBLE_OFFSETS:
-	sliced = junk[offset : offset + 8]
-	assert(len(sliced) == 8)
-	if junk[offset:offset + 8] == b'TMR SEGA':
-		print('Found TMR SEGA header at', hex(offset))
+# now search the entire rom for it
+for i in range(0, len(junk) - 8):
+	chunk = junk[i : i + 8]
+	if chunk == b'TMR SEGA':
+		print('Found TMR SEGA header at', hex(i))
+		if i not in POSSIBLE_OFFSETS:
+			print('This is an unexpected location')
 		found_it = True
 		break
 
 if not found_it:
-	print('Could not find a TMR SEGA header. Might be a Japanese ROM.')
+	print('TMR SEGA header could not be found. Maybe a Japanese ROM?')
